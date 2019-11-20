cf51eb3c2214f755d74259e71045b62a78148d8d
austinfulks <54602734+austinfulks@users.noreply.github.com>
2019-11-20 13:23:10 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+var description = document.getElementById("description")
+
+function honey() {
+	description.innerHTML = "Stephen Curry, in full Wardell Stephen Curry II, byname Steph, (born March 14, 1988, Akron, Ohio, U.S.), American professional basketball player who led the Golden State Warriors of the National Basketball Association (NBA) to championships in 2014–15, 2016–17, and 2017–18 and to the best regular-season record."
+}
+function weasel() {
+	description.innerHTML = "Willie Trill Cauley-Stein (born Willie Durmond Cauley Jr. on August 18, 1993) is an American professional basketball player for the Golden State Warriors of the National Basketball Association (NBA). He played college basketball with the Kentucky Wildcats."
+}
+function badger() {
+	description.innerHTML = "Kevin Durant, in full Kevin Wayne Durant, (born September 29, 1988, Washington, D.C., U.S.), American professional basketball player who won the 2013–14 National Basketball Association (NBA) Most Valuable Player (MVP) award and established himself as one of the best players of his generation while only in his early 20s ."
+}
+function austin() {
+	description.innerHTML = "Klay Alexander Thompson (born February 8, 1990) is an American professional basketball player for the Golden State Warriors of the National Basketball Association (NBA). He is credited as one of the greatest shooters in NBA history. "
+}
+function marten() {
+	description.innerHTML = " After Andre Iguodala entered the N.B.A., fifteen years ago, he quickly became known as an athletic scorer, playing first for the Sixers and then for the Nuggets. He made the All-Star team in 2012, and seemed destined to have a perfectly successful but championship-free career. "
+}
+function wolv() {
+	description.innerHTML = "Draymond Jamal Green Sr. (born March 4, 1990) is an American professional basketball player for the Golden State Warriors of the National Basketball Association (NBA). Green, who plays primarily at the power forward position, is a three-time NBA champion, and a three-time NBA All-Star. "
+}
+function reset(){
+	window.location.reload();
+}
